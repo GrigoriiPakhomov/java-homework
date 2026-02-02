7990a7ff7c30566ca011e33747bfa627cf8cc8a3
GrigoriiPakhomov <pakhomov-86@list.ru>
2026-02-02 14:32:04 +0800

    Задание "Вывод sin, cos, tan". Добавил обработку исключения

--- src/main/java/sinCosTan/Main.java
+++ src/main/java/sinCosTan/Main.java
@@ -8,13 +8,15 @@ public class Main {
 
         System.out.println("Введите угол в градусах: ");
         double angle = scanner.nextDouble();
+        try {
+            SinCosTan result = new SinCosTan(angle);
 
-        SinCosTan result = new SinCosTan(angle);
-
-        System.out.printf("Угол: %.1f градусов \n", angle);
-        System.out.printf("Синус: %.2f \n", result.getSin());
-        System.out.printf("Косинус: %.2f \n", result.getCos());
-        System.out.printf("Тангенс: %.2f \n", result.getTan());
-
+            System.out.printf("Угол: %.1f градусов \n", angle);
+            System.out.printf("Синус: %.2f \n", result.getSin());
+            System.out.printf("Косинус: %.2f \n", result.getCos());
+            System.out.printf("Тангенс: %.2f \n", result.getTan());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
     }
 }
