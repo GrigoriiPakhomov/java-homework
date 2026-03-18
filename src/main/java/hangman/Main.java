90d2b134a48705f86bfea8a6b0aef8bac7599a4e
GrigoriiPakhomov <pakhomov-86@list.ru>
2026-03-18 23:37:38 +0800

    Задание "Виселица"

---
+++ src/main/java/hangman/Main.java
@@ -0,0 +1,21 @@
+package main.java.hangman;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Добро пожаловать в игру \"Виселица\"!");
+            System.out.println("Загадано слово из 5 букв. У вас 7 попыток.");
+            System.out.println("Слово: _ _ _ _ _");
+            System.out.println("Использованные буквы: ");
+            System.out.println("Введите букву: ");
+
+            String input = scanner.nextLine();
+
+
+        }
+    }
+}
