aa5373832ce72cb74f90e6a65812caa0ba88efdb
GrigoriiPakhomov <pakhomov-86@list.ru>
2026-04-20 22:46:10 +0800

    Задание: Консольная битва покемонов

--- src/main/java/pokemonBattle/Pokemon.java
+++ src/main/java/pokemonBattle/Pokemon.java
@@ -2,10 +2,14 @@ package main.java.pokemonBattle;
 
 public class Pokemon {
 
+    private static final int HP_INCREASE_PER_LEVEL = 10;
+    private static final int ATTACK_INCREASE_PER_LEVEL = 2;
+
     private String name;
     private int hp;
     private int attack;
     private int level;
+    private int maxHp;
 
     public Pokemon(String name, int hp, int attack, int level) {
 
@@ -13,6 +17,7 @@ public class Pokemon {
         this.hp = hp;
         this.attack = attack;
         this.level = level;
+        this.maxHp = hp;
     }
 
     public String getName() {
@@ -38,16 +43,14 @@ public class Pokemon {
 
     public void heal(int amount) {
 
-        hp += amount;
+        hp = Math.min(maxHp, hp + amount)
     }
 
     public void levelUp() {
 
         level++;
-
-        hp += 10;
-
-        attack += 2;
+        hp += HP_INCREASE_PER_LEVEL;
+        attack += ATTACK_INCREASE_PER_LEVEL;
 
         System.out.println(name + " получил новый уровень!");
     }
